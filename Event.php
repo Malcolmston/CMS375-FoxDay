340cd34a8e30ffcc2a55c7784da0a65d2fe2c814
Malcolmston <mstone@crefeld.org>
2026-01-26 11:38:35 -0500

    fix: date stamps for sql follow non-str principle

--- Event.php
+++ Event.php
@@ -65,10 +65,10 @@ class Event extends Connect
     private function initEvents()
     {
         $events = [
-            ['title' => 'Event 1', 'date' => '2023-10-01', 'description' => 'Description for Event 1'],
-            [ 'title' => 'Event 2', 'date' => '2023-10-02', 'description' => 'Description for Event 2'],
-            [ 'title' => 'Event 3', 'date' => '2023-10-03', 'description' => 'Description for Event 3'],
-            [ 'title' => 'Event 4', 'date' => '2023-10-04', 'description' => 'Description for Event 4'],
+            ['title' => 'Event 1', 'date' => DATE('2023-10-01'), 'description' => 'Description for Event 1'],
+            ['title' => 'Event 2', 'date' => DATE('2023-10-02'), 'description' => 'Description for Event 2'],
+            ['title' => 'Event 3', 'date' => DATE('2023-10-03'), 'description' => 'Description for Event 3'],
+            ['title' => 'Event 4', 'date' => DATE('2023-10-04'), 'description' => 'Description for Event 4'],
         ];
 
         foreach($events as $event) {
