734760f5ddd9556b79b5a325d031c76fe534681e
Malcolmston <mstone@crefeld.org>
2026-01-26 11:30:17 -0500

    feat(connect): Add event initialization method

--- Connect.php
+++ Connect.php
@@ -135,6 +135,21 @@ class Connect
 
     }
 
+    private function initEvents()
+    {
+        $events = [
+            ['title' => 'Event 1', 'date' => '2023-10-01', 'description' => 'Description for Event 1'],
+            [ 'title' => 'Event 2', 'date' => '2023-10-02', 'description' => 'Description for Event 2'],
+            [ 'title' => 'Event 3', 'date' => '2023-10-03', 'description' => 'Description for Event 3'],
+            [ 'title' => 'Event 4', 'date' => '2023-10-04', 'description' => 'Description for Event 4'],
+        ];
+
+        foreach($events as $event) {
+            $this->addEvent($event['title'], $event['date'], $event['description']);
+        }
+
+    }
+
     public function __destruct()
     {
         $this->db = null;
