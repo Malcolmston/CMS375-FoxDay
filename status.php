6c4749c10f14d9383ffa0abd4ea94db0a47051a1
Malcolmston <mstone@crefeld.org>
2026-01-26 19:43:39 -0500

    feat(status.php): add MySQL status check with latency and error handling

---
+++ status.php
@@ -0,0 +1,26 @@
+<?php
+
+use Spatie\Ping\PingDestination;
+
+$mysql = PingDestination::create("127.0.0.1:3306");
+
+
+if( $mysql->isReachable() ) {
+    $latency = $mysql->getLatency();
+}
+
+$error = $mysql->getError();
+
+
+
+header("Content-Type: application/json");
+if ($error) {
+    http_response_code(401);
+    echo json_encode(['status' => 'fail', 'latency' => $latency, 'error' => $error]);
+    exit;
+}
+
+echo json_encode(['status' => 'success', 'latency' => $latency]);
+exit;
+?>
+
